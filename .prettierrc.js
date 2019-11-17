efc87faddfbab54f1230e5c2b4a58caf632ae149
zghmvp <zghmvp@163.com>
2019-11-17 10:34:23 +0800

    添加.prettierrc.js文件

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+/*
+ * @Descripttion: 文件说明
+ * @version: 1.0
+ * @Author: ZGHMVP
+ * @Date: 2019-11-08 13:14:09
+ * @LastEditors: ZGHMVP
+ * @LastEditTime: 2019-11-16 09:41:38
+ */
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: "lf"
+};
